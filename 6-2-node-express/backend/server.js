f084e07115e79754d99d910458e67ddae415606e
BaraaZino <152164051+BaraaZino@users.noreply.github.com>
2025-11-09 11:41:49 +0300

    1

--- 6-2-node-express/backend/server.js
+++ 6-2-node-express/backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { getRandomQuote } from "./quotes.js";
+import  getRandomQuote  from "./quotes.js";
 
 const app = express();
 const PORT = 3000;
